f7dfdbfb4337b3a4bc557caf5a331f48b644e9c4
Jin642 <66799827+Jin642@users.noreply.github.com>
2020-06-16 15:22:05 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+var content = document.getElementById("content");
+content.hidden = true;
+
+document.getElementById("btn").addEventListener("click", function(){
+    content.hidden = false;
+});
+
+
