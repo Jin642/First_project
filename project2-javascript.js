d4cf849809d4f2285c04169e6df259b6852b3bde
Jin642 <66799827+Jin642@users.noreply.github.com>
2020-06-16 15:39:39 -0400

    Delete project2-javascript.js

--- project2-javascript.js
+++
@@ -1,8 +0,0 @@
-var content = document.getElementById("content");
-content.hidden = true;
-
-document.getElementById("btn").addEventListener("click", function(){
-    content.hidden = false;
-});
-
-
